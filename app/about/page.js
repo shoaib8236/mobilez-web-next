2d7aaa0092c632339d1f7610ffc04a48cc85c0e3
shoaib8236 <shoaib.raza.dev@gmail.com>
2023-11-22 08:43:18 -0800

    styled button created

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,17 @@
+import StyledButton from "@/app-ui/StyledButton/StyledButton";
 import React from "react";
 
 const Page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <StyledButton className="primary">Click me</StyledButton>
+      <StyledButton className="secondary">Click me</StyledButton>
+      <StyledButton className="danger">Click me</StyledButton>
+      <StyledButton className="info">Click me</StyledButton>
+      <StyledButton className="warning">Click me</StyledButton>
+      <StyledButton className="success">Click me</StyledButton>
+    </div>
+  );
 };
 
 export default Page;
