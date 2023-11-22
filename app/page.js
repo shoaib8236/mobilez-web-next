1d4ac7bae497e84c1c97693c40f30318e934d841
shoaib8236 <shoaib.raza.dev@gmail.com>
2023-11-22 07:13:04 -0800

    setup completed

--- app/page.js
+++ app/page.js
@@ -4,20 +4,14 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <Image
+      <Image
         className={styles.logo}
         src="/next.svg"
         alt="Next.js Logo"
         width={180}
         height={37}
         priority
-      /> */}
-      <h1>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi magni
-        atque necessitatibus! Minus quis adipisci nemo voluptas incidunt.
-        Facilis hic, minus consequuntur quas quos ducimus eligendi mollitia
-        asperiores consequatur dignissimos.
-      </h1>
+      />
     </main>
   );
 }
