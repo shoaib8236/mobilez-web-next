69c3091948e6c0f57f700604cd3ff8d1c4c5d07e
shoaib8236 <shoaib.raza.dev@gmail.com>
2023-11-23 10:50:33 -0800

    getFormattedDate helper

--- utils/helper.js
+++ utils/helper.js
@@ -1,5 +1,15 @@
+import moment from "moment/moment";
+
 export const getImage = (endPoint) => {
   if (endPoint) {
     return `https://www.mobilezmarket.com/images/${endPoint}`;
   } else return "";
+};
+
+export const getFormattedDate = (date, form) => {
+  if (form) {
+    return moment(date).format(form);
+  } else {
+    return moment(date).format("DD MMM YYYY");
+  }
 };
