cac2b14104f2774cfcaf7dac41ff9422cfee3cae
shoaib8236 <shoaib.raza.dev@gmail.com>
2023-11-24 09:01:22 -0800

    navbar styles

--- app-ui/Navbar/Navbar.jsx
+++ app-ui/Navbar/Navbar.jsx
@@ -1,64 +1,73 @@
-'use client'
+"use client";
 
 import React, { useRef, useEffect, useCallback } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import  StyledButton  from "../StyledButton/StyledButton";
+import StyledButton from "../StyledButton/StyledButton";
 import { AiOutlineMenu } from "react-icons/ai";
-import {  usePathname } from "next/navigation";
-
+import { usePathname } from "next/navigation";
 
 const Navbar = (props) => {
+  const menuRef = useRef(null);
+  const linkRef = useRef(null);
+  const pathname = usePathname();
 
-  const menuRef = useRef(null)
-  const linkRef = useRef(null)
-  const pathname = usePathname()
-  
   const onOpenMobileMenu = useCallback(() => {
     if (menuRef.current && linkRef.current) {
       if (menuRef.current.classList.contains("open_menu")) {
-        menuRef.current.classList.remove("open_menu")
-        linkRef.current.classList.remove("animate_links")
+        menuRef.current.classList.remove("open_menu");
+        linkRef.current.classList.remove("animate_links");
       } else {
-        menuRef.current.classList.add("open_menu")
-        linkRef.current.classList.add("animate_links")
+        menuRef.current.classList.add("open_menu");
+        linkRef.current.classList.add("animate_links");
       }
     }
-  }, [menuRef, linkRef])
+  }, [menuRef, linkRef]);
 
-
-  useEffect(()=> {
-
+  useEffect(() => {
     if (menuRef.current && linkRef.current) {
       if (menuRef.current.classList.contains("open_menu")) {
-        menuRef.current.classList.remove("open_menu")
-        linkRef.current.classList.remove("animate_links")
-      } 
+        menuRef.current.classList.remove("open_menu");
+        linkRef.current.classList.remove("animate_links");
+      }
     }
-
-  }, [pathname, menuRef, linkRef])
+  }, [pathname, menuRef, linkRef]);
 
   return (
     <nav className="nav_wrapper">
       <div className="nav_content">
         <div className="logo">
-        <Image src="/logo.png" alt="logo" width={140} height={62} />
+          <Image src="/logo.png" alt="logo" width={140} height={60} />
         </div>
         <div ref={menuRef} className="links_container">
           <ul ref={linkRef}>
-           
-           <li><Link href="/">Home</Link></li>
-           <li><Link href="/about">About</Link></li>
-           <li><Link href="/blogs">Blogs</Link></li>
-           <li><Link href="/careers">Careers</Link></li>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/blogs">Blogs</Link>
+            </li>
+            <li>
+              <Link href="/careers">Careers</Link>
+            </li>
           </ul>
           <div className="search_container">
             {/* <StyledSearch />  */}
-            <StyledButton className="login_btn primary">Login</StyledButton>
-            <StyledButton className="register_btn secondary">Register</StyledButton>
+            <StyledButton className="login_btn light">Sign in</StyledButton>
+            <StyledButton className="register_btn primary">
+              Register
+            </StyledButton>
           </div>
         </div>
-        <div className="mobile_actions"> <StyledButton onClick={onOpenMobileMenu} className="nav_open_btn"><AiOutlineMenu /></StyledButton> </div>
+        <div className="mobile_actions">
+          {" "}
+          <StyledButton onClick={onOpenMobileMenu} className="nav_open_btn">
+            <AiOutlineMenu />
+          </StyledButton>{" "}
+        </div>
       </div>
     </nav>
   );
